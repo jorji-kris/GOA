9f7e9b40e8425bcbb28466773f2626948a07a95f
jorji-kris <gioxx243@gmail.com>
2024-10-10 22:55:25 +0400

    first commit

---
+++ level06/classwork/.py
@@ -0,0 +1,13 @@
+#1)
+name = input("whats ur name ? " )
+surname = input("whats ur surname ? ")
+print(name, surname)
+#2)
+name1 = int(input("name1: "))
+name2 = int(input("name2: "))
+print(name1 + name2, name1 * name2, name1 / name2)
+#print("float", 15 / 4)
+#print("int", 15 // 4)
+
+#$ mkdir
+#$ Touch
---
+++ level06/homework/.py
@@ -0,0 +1,11 @@
+age = int(input("enter ur age: "))
+
+x = int(input("enter x for square: "))
+y = int(input("enter y for square: "))
+print("s = ", x * y)
+print("p = ", 2*(x+y))
+
+a = int(input("enter ur distance from school in meters: "))
+print(a)
+print(a * 100, "cm")
+print(a * 100, "mm")
